9b105047ca5db91674cb490df42a50861421b3c8
Clarence Du <du.du@fomopay.com>
2025-02-13 15:07:04 +0800

    feat: init chapter1

---
+++ chapter1/chapter1.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+
+# 1.3
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+
+
+# 1.4 找到最大或最小的N个元素
+import heapq
+
+nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+print(heapq.nlargest(3, nums))
+print(heapq.nsmallest(3, nums))
